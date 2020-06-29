76c03e31f793292a6eda33fa78cfaf50a1633f9e
wcshamblin <will@shamblin.org>
2020-06-28 21:23:18 -0400

    Load config file

--- shbm_api/management/commands/heartmonitor.py
+++ shbm_api/management/commands/heartmonitor.py
@@ -3,6 +3,11 @@ from ...serializers import hostserializer
 from ...models import host
 from time import sleep, time
 from subprocess import Popen
+from json import load
+
+conffile = open("./config.json", "r")
+config = load(conffile)
+
 
 class Command(BaseCommand):
     def handle(self, **options):
@@ -10,7 +15,7 @@ class Command(BaseCommand):
             hosts=hostserializer(host.objects.all(), many=True).data
             for machine in hosts:
                 if machine['active'] and not machine['down']:
-                    if time()-10 > machine['heartrate']:
+                    if time()-config['wait-seconds'] > machine['heartrate']:
                         Popen(["./alerting.py", machine['hostname'], machine['ip']])
                         req = host.objects.get(pk=machine['idstr'])
                         req.down = True
