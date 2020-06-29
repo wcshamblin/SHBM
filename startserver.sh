7b52bb49784493ae18ab9aa8fce39a71926b4d73
wcshamblin <will@shamblin.org>
2020-06-29 15:02:58 -0400

    Run on 0.0.0.0

--- startserver.sh
+++ startserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dir=$(dirname `dirname "$0"`)
-tmux new-session -d -n "SHBM" python3 $dir/manage.py runserver & python3 $dir/manage.py heartmonitor &
+tmux new-session -d -n "SHBM" python3 $dir/manage.py runserver 0.0.0.0:8000 & python3 $dir/manage.py heartmonitor &
